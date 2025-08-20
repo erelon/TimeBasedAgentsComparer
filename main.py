17c26f4341cab07c60d5da4e0d7f92e713549095
Erel shtossel <erelon39@gmail.com>
2025-08-20 17:52:14 +0300

    fixed random chooses in agent

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def train_single_agent(agent, env, episodes=200, eval_steps=20, seed=42):
     """
     env.set_seed(seed)
     env.reset()
+    agent.seed = seed
+    agent.reset()
+
     state = env.get_state()  # In a stateless environment, state is not used
     for episode in range(episodes):
         action = agent.act(state)
